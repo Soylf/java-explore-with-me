9494de686e23bf60cda40af4118198c0bb2f6c61
Soyl <Soylgr@yandex.ru>
2024-03-31 10:39:46 +0300

    fix

--- stat/client/src/main/java/ru/practicum/stats/config/RestTemplateConfig.java
+++ stat/client/src/main/java/ru/practicum/stats/config/RestTemplateConfig.java
@@ -1,16 +1,15 @@
 package ru.practicum.stats.config;
 
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
-import org.springframework.stereotype.Component;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
-@Component
+@Configuration
 public class RestTemplateConfig {
 
-
     @Bean
-    public RestTemplate restTemplate() {
-        return new RestTemplate();
-
+    public RestTemplate restTemplate(RestTemplateBuilder builder) {
+        return builder.build();
     }
 }
