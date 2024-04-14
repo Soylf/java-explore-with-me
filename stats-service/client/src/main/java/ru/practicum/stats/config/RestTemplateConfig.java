6ef03842defb66d962c4b80736891d61423b1251
Soyl <Soylgr@yandex.ru>
2024-04-14 19:50:37 +0300

    fix/info: я устал

--- stats-service/client/src/main/java/ru/practicum/stats/config/RestTemplateConfig.java
+++ stats-service/client/src/main/java/ru/practicum/stats/config/RestTemplateConfig.java
@@ -3,9 +3,7 @@ package ru.practicum.stats.config;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
-import ru.practicum.stats.client.StatClient;
 
 @Configuration
 public class RestTemplateConfig {
